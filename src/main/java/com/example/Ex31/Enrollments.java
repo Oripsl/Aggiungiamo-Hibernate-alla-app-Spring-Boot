ed1f7e68a2d06ac57967e0b2f09bb1a11dddddf6
Basile Bartolomeo <basilebartolomeo01@gmail.com>
2024-04-16 14:52:37 +0200

    first commit

---
+++ src/main/java/com/example/Ex31/Enrollments.java
@@ -0,0 +1,21 @@
+package com.example.Ex31;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "enrollments")
+public class Enrollments {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @ManyToOne
+    @JoinColumn(name = "student_id", nullable = false)
+    private Student student;
+
+    @ManyToOne
+    @JoinColumn(name = "class_id", nullable = false)
+    private Classes classes;
+}
